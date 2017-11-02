a5fc0bfbde635f2beddc27b2f384130b74df6c4b
Lasana Murray <lmurray@quenk.com>
2017-11-02 13:47:27 -0400

    fix: export all Table members

--- src/table/table/index.ts
+++ src/table/table/index.ts
@@ -1,5 +1,8 @@
-export {CellClickedEvent} from './CellClickedEvent';
-export {HeadingClickedEvent} from './HeadingClickedEvent';
-export {RowClickedEvent} from './RowClickedEvent';
-export {RowSelectedEvent} from './RowSelectedEvent';
-export {Table,Comparable,SortingStrategy,Field} from './Table';
+export { CellClickedEvent } from './CellClickedEvent';
+export { HeadingClickedEvent } from './HeadingClickedEvent';
+export { RowClickedEvent } from './RowClickedEvent';
+export { RowSelectedEvent } from './RowSelectedEvent';
+export {
+    Table, Comparable, SortingStrategy, Field, CellFragment,
+    TableModel, DefaultTableModel, SortTableModel, TableAttrs
+} from './Table';
