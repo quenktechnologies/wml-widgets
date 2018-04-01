6f21779dad5d89b895a21a5beb9f0b4c00a7ad38
Lasana Murray <lmurray@quenk.com>
2018-04-01 02:39:30 -0400

    refactor(BusyIndicator): use classNames

--- src/app/busy-indicator/index.ts
+++ src/app/busy-indicator/index.ts
@@ -1,1 +1,30 @@
-export {BusyIndicator} from './BusyIndicator';
+import * as wml from '@quenk/wml';
+import { Main } from './wml/busy_indicator';
+import { concat } from '../../util';
+
+export interface BusyIndicatorAttrs extends wml.Attrs {
+
+    ww: {
+
+        /**
+         * class name to append to the root element.
+         */
+        class?: string
+    }
+
+}
+
+/**
+ * BusyIndicator provides a 'hamburger' menu button.
+ */
+export class BusyIndicator extends wml.Component<BusyIndicatorAttrs> {
+
+    view: wml.View = new Main(this);
+
+    values = {
+
+        class: concat('loading', this.attrs.ww ? this.attrs.ww.class : '')
+
+    }
+
+}
