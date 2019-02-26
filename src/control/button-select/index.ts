b6e5cbfffcc8dac016c9a55e8962a92c67f22dea
Lasana Murray <lmurray@quenk.com>
2019-02-26 12:22:43 -0400

    refactor: update button select

---
+++ src/control/button-select/index.ts
@@ -0,0 +1,234 @@
+import * as views from './wml/button-select';
+import { View } from '@quenk/wml';
+import { Style } from '../../content/style';
+import { concat } from '../../util';
+import { getId, getClassName } from '../../';
+import { ControlAttrs, Event, AbstractControl } from '../';
+
+///className:begin
+export const BUTTON_SELECT = 'ww-button-select';
+export const BUTTON_SELECT_OPTION = 'ww-button-select__option';
+///className:end
+
+/**
+ * Option provides the information for rendering button select options.
+ */
+export interface Option<V> {
+
+    /**
+     * value provided when the option's button has been clicked.
+     */
+    value: V,
+
+    /**
+     * text displayed for the button.
+     */
+    text: string,
+
+    /**
+     * className to add to the rendered button.
+     */
+    className?: string
+
+}
+
+/**
+ * ButtonSelectAttrs
+ */
+export interface ButtonSelectAttrs<O, V> extends ControlAttrs<V> {
+
+    /**
+     * options to display
+     */
+    options: Option<O>[],
+
+    /**
+     * style in style to use.
+     */
+    style?: Style,
+
+    /**
+     * onChange handler.
+     */
+    onChange?: (e: ButtonChangedEvent<V>) => void
+
+}
+
+/**
+ * ButtonChangedEvent
+ */
+export class ButtonChangedEvent<V> extends Event<V>{ }
+
+/**
+ * ButtonSelectInterface 
+ */
+export interface ButtonSelectInterface<V> {
+
+    /**
+     * values available to the View's template.
+     */
+    values: {
+
+        /**
+         * root element values.
+         */
+        root: {
+
+            /**
+             * id of the root element
+             */
+            id: string,
+
+            /**
+             * className of the root element.
+             */
+            className: string
+
+        },
+
+        /**
+         * buttons values.
+         */
+        buttons: {
+
+            /**
+             * options used to display the buttons
+             */
+            options: Option<V>[],
+
+            /**            
+             * click is applied to the value of an option's value when 
+             * it is clicked by the user.
+             */
+            click: (n: number) => void,
+
+            /**
+             * getClassNames for an options' button.
+             */
+            getClassNames: (n: number) => string
+
+            /**
+             * getStyle
+             */
+            getStyle: () => Style,
+
+            /**
+             * getActive
+             */
+            getActive: (n: number) => boolean
+
+        }
+
+    }
+
+}
+
+/**
+ * ButtonSelect
+ */
+export class ButtonSelect<V> extends AbstractControl<V, ButtonSelectAttrs<V, V>> {
+
+    view: View = new views.Main(this);
+
+    values = {
+
+        root: {
+
+            id: getId(this.attrs),
+
+            className: concat(BUTTON_SELECT, getClassName(this.attrs))
+
+        },
+        buttons: {
+
+            current: -1,
+
+            options: (this.attrs.ww && this.attrs.ww.options) ?
+                this.attrs.ww.options : [],
+
+
+            click: (idx: number) => {
+
+                this.values.buttons.current = idx;
+
+                if ((this.attrs.ww && this.attrs.ww.onChange))
+                    this.attrs.ww.onChange(
+                        new ButtonChangedEvent(<string>this.attrs.ww.name,
+                            this.values.buttons.options[idx].value));
+
+                this.view.invalidate();
+
+            },
+            getStyle: () => (this.attrs.ww && this.attrs.ww.style) ?
+                this.attrs.ww.style : Style.Default,
+
+            getActive: (n: number) => this.values.buttons.current === n,
+
+            getClassNames: (n: number) =>
+                concat(BUTTON_SELECT_OPTION,
+                    <string>this.values.buttons.options[n].className)
+
+        }
+
+    }
+
+}
+
+/**
+ * MultiButtonSelect
+ */
+export class MultiButtonSelect<V>
+    extends AbstractControl<V[], ButtonSelectAttrs<V, V[]>> {
+
+    view: View = new views.Main(this);
+
+    values = {
+
+        root: {
+
+            id: getId(this.attrs),
+
+            className: concat(BUTTON_SELECT, getClassName(this.attrs))
+
+        },
+        buttons: {
+
+            values: <number[]>[],
+
+            options: (this.attrs.ww && this.attrs.ww.options) ?
+                this.attrs.ww.options : [],
+
+
+            click: (n: number) => {
+
+                let values = this.values.buttons.values;
+                let pos = values.indexOf(n);
+
+                if (pos > -1)
+                    values.splice(pos, 1);
+                else
+                    values.push(n);
+
+                if (this.attrs.ww && this.attrs.ww.onChange)
+                    this.attrs.ww.onChange(new ButtonChangedEvent(
+                        <string>this.attrs.ww.name,
+                        values.map(n => this.values.buttons.options[n].value)));
+
+              this.view.invalidate();
+
+            },
+
+            getStyle: () => (this.attrs.ww && this.attrs.ww.style) ?
+                this.attrs.ww.style : Style.Default,
+
+            getActive: (n: number) => this.values.buttons.values.indexOf(n) > -1,
+
+            getClassNames: (n: number) =>
+                concat(BUTTON_SELECT_OPTION,
+                    <string>this.values.buttons.options[n].className)
+
+        }
+
+    }
+
+}
---
+++ test/browser/app/page/control_button-select/index.ts
@@ -0,0 +1,54 @@
+import * as wml from '@quenk/wml';
+import * as views from './wml/button-select'
+import { Option, ButtonChangedEvent } from '../../../../../lib/control/button-select';
+
+export class ButtonSelectPage {
+
+    view: wml.View = new views.Main(this);
+
+    values = {
+
+        options: <Option<string>[]>[
+            { text: 'Asus', value: 'Asus' },
+            { text: 'MSI', value: 'MSI' },
+            { text: 'Gigabyte', value: 'Gigabyte' }]
+
+    };
+
+    onChange: (e: ButtonChangedEvent<string>) => void =
+        ({ value, name }: ButtonChangedEvent<string>) => {
+
+            this
+                .view
+                .findById<HTMLElement>(`${name}-content`)
+                .map((e: HTMLElement) => {
+
+                    while (e.lastChild)
+                        e.removeChild(e.lastChild);
+
+                    e.appendChild(document.createTextNode(String(value)));
+
+                });
+
+        }
+
+    onChangeMulti: (e: ButtonChangedEvent<string[]>) => void =
+        ({ value, name }: ButtonChangedEvent<string[]>) => {
+
+            this
+                .view
+                .findById<HTMLElement>(`${name}-content`)
+                .map((e: HTMLElement) => {
+
+                    while (e.lastChild)
+                        e.removeChild(e.lastChild);
+
+                    e.appendChild(document.createTextNode(String(value)));
+
+                });
+
+        }
+
+}
+
+export default new ButtonSelectPage()
