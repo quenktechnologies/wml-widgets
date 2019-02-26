7ddd638373d59e908efbc3c9b13f2c187f7930ec
Lasana Murray <lmurray@quenk.com>
2019-02-26 08:11:48 -0400

    refactor: update button group

---
+++ src/control/button-group/index.ts
@@ -0,0 +1,43 @@
+import * as views from './wml/button-group';
+import { View, Content, Component } from '@quenk/wml';
+import { concat } from '../../util';
+import { TOOLBAR_COMPAT } from '../toolbar';
+import { WidgetAttrs, HTMLElementAttrs, getId, getClassName } from '../../';
+
+///classNames:begin
+export const BUTTON_GROUP = 'ww-button-group';
+///classNames:end
+
+/**
+ * ButtonGroupAttrs 
+ */
+export interface ButtonGroupAttrs extends HTMLElementAttrs {
+
+    /**
+   * content can be specified instead of the children attribute.
+   */
+    content?: Content[]
+
+}
+
+/**
+ * ButtonGroup groups multiple buttons into one element.
+ */
+export class ButtonGroup extends Component<WidgetAttrs<ButtonGroupAttrs>> {
+
+    view: View = new views.Main(this);
+
+    values = {
+
+        root: {
+
+            id: getId(this.attrs),
+
+          className: concat(BUTTON_GROUP, TOOLBAR_COMPAT, 
+            getClassName(this.attrs))
+
+        }
+
+    }
+
+}
