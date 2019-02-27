f323f6e4aa7ab1d64cfde32817f1adad5ee218db
Lasana Murray <lmurray@quenk.com>
2019-02-27 10:02:31 -0400

    refactor: update toolbar

---
+++ src/control/toolbar/index.ts
@@ -0,0 +1,37 @@
+import * as views from './wml/toolbar';
+import {View, Component} from '@quenk/wml';
+import { concat } from '../../util';
+import {WidgetAttrs, HTMLElementAttrs, getId, getClassName} from '../../';
+import { ToolbarAttrs } from '.';
+
+///classNames:begin
+export const TOOLBAR = 'ww-toolbar';
+export const TOOLBAR_COMPAT = '-toolbar-compat';
+///classNames:end
+
+/**
+ * ToolbarAttrs
+ */
+export interface ToolbarAttrs extends HTMLElementAttrs {}
+
+/**
+ * Toolbar provides a widget for grouping related controls into a
+ * single row.
+ */
+export class Toolbar extends Component<WidgetAttrs<ToolbarAttrs>> {
+
+    view: View = new views.Main(this);
+
+    values = {
+
+        root: {
+
+          id: getId(this.attrs),
+          
+          className: concat(TOOLBAR, getClassName(this.attrs))
+
+        }
+
+    }
+
+}
