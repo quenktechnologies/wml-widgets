118581c3d38184d8f9e7d28b01eac0fc48ce6c36
Lasana Murray <lmurray@quenk.com>
2019-02-27 10:01:00 -0400

    refactor: update checkbox

---
+++ src/control/checkbox/index.ts
@@ -0,0 +1,74 @@
+import { View } from '@quenk/wml';
+import { Main } from './wml/checkbox';
+import {concat} from '../../util';
+import { ControlAttrs, AbstractControl, Event,getName } from '../';
+import {getId, getClassName} from '../../';
+
+///classNames:begin
+export const CHECKBOX = 'ww-checkbox';
+///classNames:end
+
+/**
+ * CheckboxAttrs
+ */
+export interface CheckboxAttrs extends ControlAttrs<boolean> {
+
+    /**
+     * disabled checkbox.
+     */
+    disabled?: boolean,
+
+    /**
+     * onChange handler.
+     */
+    onChange?: (e: CheckChangedEvent) => void
+
+}
+
+/**
+ * CheckChangedEvent signals the user has changed the checkbox state.
+ */
+export class CheckChangedEvent extends Event<boolean> { }
+
+/**
+ * Checkbox control.
+ *
+ * This is an alternative to the native checkbox that can be styled.
+ */
+export class Checkbox extends AbstractControl<boolean, CheckboxAttrs> {
+
+    view: View = new Main(this);
+
+    values = {
+
+      root: {
+
+          id: getId(this.attrs),
+
+            className: concat(CHECKBOX,getClassName(this.attrs))
+
+        },
+        input: {
+
+          name: getName(this.attrs),
+
+          value: (this.attrs.ww && this.attrs.ww.value) ?
+          this.attrs.ww.value : null,
+
+            onChange: () => {
+
+                this.values.input.value = (!this.values.input.value) || null;
+
+                if (this.attrs.ww && this.attrs.ww.onChange)
+                    this.attrs.ww.onChange(
+                        new CheckChangedEvent(
+                            this.values.input.name,
+                            this.values.input.value || false));
+
+            }
+        }
+
+    };
+
+}
+
