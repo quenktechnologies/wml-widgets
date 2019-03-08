3eb8685d1cdb150047fde709989d345e49b09b9c
Lasana Murray <lmurray@quenk.com>
2019-03-07 22:49:58 -0400

    refactor(layout/horizontal): allow orientation to be changed

--- src/classNames.ts
+++ src/classNames.ts
@@ -40,6 +40,16 @@ export const CLEARFIX = '-clearfix';
  * JUSTIFIED content.
  */
 export const JUSTIFIED = '-justified';
+
+/**
+ * LEFT indicates content floated or positioned to the left.
+ */
+export const LEFT = '-left';
+
+/**
+ * RIGHT indicates content floated or positioned to the right.
+ */
+export const RIGHT = '-right';
 ///classNames:end
 ///classNames:begin
 /**
