e03dc270ca895f435e43269a226f5d8672951cba
Lasana Murray <lmurray@quenk.com>
2019-07-14 13:06:22 -0400

    refactor(control): new better mutlselect

--- src/control/search/index.ts
+++ src/control/search/index.ts
@@ -10,9 +10,14 @@ import {
     ItemSelectedEvent
 } from '../results-menu';
 import { getId, getClassName } from '../../';
-import { ControlAttrs, Event, AbstractControl } from '../';
+import { ControlAttrs, Event, AbstractControl, getName } from '../';
 
-export { ItemSelectedEvent }
+export {
+    Stringifier,
+    ItemTemplate,
+    NoItemsTemplate,
+    ItemSelectedEvent
+}
 
 /**
  * ESCAPE key code.
@@ -25,9 +30,9 @@ export const SEARCH_INPUT = 'ww-search__input';
 ///classNames:end
 
 /**
- * SearchAttrs
+ * CommonAttrs
  */
-export interface SearchAttrs<V> extends ControlAttrs<V> {
+export interface CommonAttrs<V> extends ControlAttrs<V> {
 
     /**
      * onSearch handler.
@@ -35,36 +40,20 @@ export interface SearchAttrs<V> extends ControlAttrs<V> {
     onSearch?: (e: TermChangedEvent) => void
 
     /**
-     * onEscape handler.
-     */
-    onEscape?: () => void,
-
-    /**
      * onFocus handler.
      */
     onFocus?: (e: FocusGainedEvent) => void,
 
     /**
-     * onSelect is applied when the user selects an item.
+     * onBlur handler
      */
-    onSelect?: (e: ItemSelectedEvent<V>) => void,
+    onBlur?: (e: FocusLostEvent) => void,
 
     /**
      * stringifier turns item values into a string so that 
      * they can be used used as labels.
      */
     stringifier?: Stringifier<V>,
-
-    /**
-     * itemTemplate if specified will be used to render each
-     * result item.
-     */
-    itemTemplate?: ItemTemplate<V>,
-
-    /**
-     * noItemsTemplate for rendering the lack of search results.
-     */
-    noItemsTemplate?: NoItemsTemplate,
 
     /**
      * placeholder
@@ -81,6 +70,46 @@ export interface SearchAttrs<V> extends ControlAttrs<V> {
      */
     block?: boolean,
 
+}
+
+/**
+ * InputAttrs
+ */
+export interface InputAttrs<V> extends CommonAttrs<V> {
+
+    /**
+     * size of the input.
+     */
+    size?: number,
+
+    /**
+     * onEscape handler.
+     */
+    onEscape?: (e: EscapeEvent) => void,
+
+}
+
+/**
+ * SearchAttrs
+ */
+export interface SearchAttrs<V> extends CommonAttrs<V> {
+
+    /**
+     * onSelect is applied when the user selects an item.
+     */
+    onSelect?: (e: ItemSelectedEvent<V>) => void,
+
+    /**
+     * itemTemplate if specified will be used to render each
+     * result item.
+     */
+    itemTemplate?: ItemTemplate<V>,
+
+    /**
+     * noItemsTemplate for rendering the lack of search results.
+     */
+    noItemsTemplate?: NoItemsTemplate,
+
     /**
      * term value to display
      */
@@ -94,11 +123,144 @@ export interface SearchAttrs<V> extends ControlAttrs<V> {
 export class TermChangedEvent extends Event<string>{ }
 
 /**
- * FocusGainedEvent singlas the user as given focus to the search.
+ * FocusGainedEvent signals the user as given focus to the control.
  */
 export class FocusGainedEvent {
 
     constructor(public name: string) { }
+
+}
+
+/**
+ * FocusLostEvent signals the user has removed focus from the control.
+ */
+export class FocusLostEvent {
+
+    constructor(public name: string) { }
+
+}
+
+/**
+ * EscapeEvent signals the user has pressed the escape key while typing.
+ */
+export class EscapeEvent {
+
+    constructor(public name: string) { }
+
+}
+
+/**
+ * Input provides the input part of a Search control.
+ *
+ * This is here mostly for code re-use.
+ */
+export class Input
+    extends
+    AbstractControl<string, InputAttrs<string>> {
+
+    view: View = new views.InputView(this);
+
+    values = {
+
+        wml: { id: 'root' },
+
+        className: concat(SEARCH_INPUT, getClassName(this.attrs)),
+
+        placeholder: (this.attrs.ww && this.attrs.ww.placeholder) ?
+            this.attrs.ww.placeholder : '',
+
+        readOnly: (this.attrs.ww && this.attrs.ww.readOnly) || undefined,
+
+        autocomplete: 'do not enable please',
+
+        size: String((this.attrs.ww && this.attrs.ww.size) ?
+            this.attrs.ww.size : 20),
+
+        value: (this.attrs.ww && this.attrs.ww.value) ?
+            this.attrs.ww.value : '',
+
+        onfocus: (e: KeyboardEvent) => {
+
+            let target = <HTMLInputElement>e.target;
+
+            if (this.attrs.ww && this.attrs.ww.onFocus)
+                this.attrs.ww.onFocus(new FocusGainedEvent(
+                    this.attrs.ww && this.attrs.ww.name || ''));
+
+            target.value = target.value;
+
+        },
+        onkeydown: (e: KeyboardEvent) => {
+
+            if (e.keyCode !== ESCAPE)
+                if (this.attrs.ww && this.attrs.ww.onSearch) {
+
+                    let name = '' + this.attrs.ww.name;
+                    let value = (<HTMLInputElement>e.target).value;
+
+                    this.attrs.ww.onSearch(new TermChangedEvent(name, value));
+
+                }
+
+        },
+        onkeyup: (e: KeyboardEvent) => {
+
+            if (e.keyCode === ESCAPE)
+                if (this.attrs.ww && this.attrs.ww.onEscape)
+                    this.attrs.ww.onEscape(
+                        new EscapeEvent(this.attrs.ww.name || ''));
+
+        },
+        oninput: (e: KeyboardEvent) => {
+
+            //For compatability reasons
+            (<HTMLInputElement>e.target).oninput = null;
+            this.values.onkeydown(e);
+
+        },
+        onblur: () => {
+
+            if (this.attrs.ww && this.attrs.ww.onBlur)
+                this.attrs.ww.onBlur(new FocusLostEvent(
+                    this.attrs.ww && this.attrs.ww.name || ''));
+
+        }
+
+    }
+
+    /**
+     * focus steals user focus to the Input.
+     */
+    focus(): Input {
+
+        getInput(this).map(i => i.focus());
+        return this;
+
+    }
+
+
+    /**
+     * setSize sets the size of the input.
+     */
+    setSize(n: number): Input {
+
+        this.values.size = String(n);
+
+        getInput(this).map(i => i.setAttribute('size', String(n)));
+
+        return this;
+
+    }
+
+    /**
+     * getSize returns the size of the (internally tracked) size
+     * of the Input. 
+     */
+    getSize(): number {
+
+        return Number(this.values.size);
+
+    }
 
 }
 
@@ -135,12 +297,12 @@ export class Search<V> extends AbstractControl<V, SearchAttrs<V>> {
 
             },
 
-            className: SEARCH_INPUT,
+            name: getName(this.attrs),
 
             placeholder: (this.attrs.ww && this.attrs.ww.placeholder) ?
                 this.attrs.ww.placeholder : '',
 
-            readOnly: (this.attrs.ww && this.attrs.ww.readOnly) || null,
+            readOnly: (this.attrs.ww && this.attrs.ww.readOnly) || undefined,
 
             value: (this.attrs.ww && this.attrs.ww.term) ?
                 this.attrs.ww.term :
@@ -149,51 +311,15 @@ export class Search<V> extends AbstractControl<V, SearchAttrs<V>> {
                     this.attrs.ww.value) ?
                     this.attrs.ww.stringifier(this.attrs.ww.value) : '',
 
-            onfocus: (e: KeyboardEvent) => {
+            onFocus: this.attrs.ww && this.attrs.ww.onFocus || undefined,
 
-                let target = <HTMLInputElement>e.target;
+            onSearch: (this.attrs.ww && this.attrs.ww.onSearch) ?
+                this.attrs.ww.onSearch : undefined,
 
-                if (this.attrs.ww && this.attrs.ww.onFocus)
-                    this.attrs.ww.onFocus(new FocusGainedEvent(
-                        this.attrs.ww && this.attrs.ww.name || ''));
+            onEscape: () => this.close(),
 
-                target.value = target.value;
-
-            },
-            onkeydown: (e: KeyboardEvent) => {
-
-                if (e.keyCode === ESCAPE) {
-
-                    if (this.attrs.ww && this.attrs.ww.onEscape)
-                        this.attrs.ww.onEscape();
-
-                } else {
-
-                    if (this.attrs.ww && this.attrs.ww.onSearch) {
-
-                        let name = '' + this.attrs.ww.name;
-                        let value = (<HTMLInputElement>e.target).value;
-
-                        this.attrs.ww.onSearch(new TermChangedEvent(name, value));
-
-                    }
-
-                }
-
-            },
-            onkeyup: (e: KeyboardEvent) => {
-
-                if (e.keyCode === ESCAPE)
-                    (<HTMLInputElement>e.target).blur();
-
-            },
-            oninput: (e: KeyboardEvent) => {
-
-                //For compatability reasons
-                (<HTMLInputElement>e.target).oninput = null;
-                this.values.input.onkeydown(e);
-
-            }
+            onBlur: (this.attrs.ww && this.attrs.ww.onBlur) ?
+                this.attrs.ww.onBlur : undefined
 
         },
         menu: {
@@ -209,8 +335,6 @@ export class Search<V> extends AbstractControl<V, SearchAttrs<V>> {
 
             block: (this.attrs.ww && this.attrs.ww.block) ?
                 this.attrs.ww.block : false,
-
-            options: <V[]>[],
 
             onSelect: (e: ItemSelectedEvent<V>) => {
 
@@ -248,11 +372,7 @@ export class Search<V> extends AbstractControl<V, SearchAttrs<V>> {
      */
     update(results: V[]): Search<V> {
 
-        let mMenu = getById<ResultsMenu<V>>(this.view, this.values.menu.wml.id);
-
-        if (mMenu.isJust())
-            mMenu.get().update(results);
-
+        updateMenu(this.view, this.values.menu.wml.id, results);
         return this;
 
     }
@@ -262,9 +382,7 @@ export class Search<V> extends AbstractControl<V, SearchAttrs<V>> {
      */
     open(): Search<V> {
 
-        getById<ResultsMenu<V>>(this.view, this.values.menu.wml.id)
-            .map((m: ResultsMenu<V>) => m.open());
-
+        openMenu(this.view, this.values.menu.wml.id);
         return this;
 
     }
@@ -274,12 +392,45 @@ export class Search<V> extends AbstractControl<V, SearchAttrs<V>> {
      */
     close(): Search<V> {
 
-        getById<ResultsMenu<V>>(this.view, this.values.menu.wml.id)
-            .map((m: ResultsMenu<V>) => m.close());
-
+        closeMenu(this.view, this.values.menu.wml.id);
         return this;
 
     }
 
+}
+
+const getInput = (i: Input) =>
+    getById<HTMLInputElement>(i.view, i.values.wml.id);
+
+/**
+ * updateMenu will cause the menu to be displayed with new items.
+ */
+export const updateMenu = <V>(view: View, id: string, results: V[]): void => {
+
+    let mMenu = getById<ResultsMenu<V>>(view, id);
+
+    if (mMenu.isJust())
+        mMenu.get().update(results);
+
+}
+
+/**
+ * openMenu will cause the menu to be displayed with whatever contents
+ * were previously set.
+ */
+export const openMenu = <V>(view: View, id: string): void => {
+
+    getById<ResultsMenu<V>>(view, id)
+        .map((m: ResultsMenu<V>) => m.open());
+
+}
+
+/**
+ * closeMenu will cause the menu to no longer be displayed.
+ */
+export const closeMenu = <V>(view: View, id: string): void => {
+
+    getById<ResultsMenu<V>>(view, id)
+        .map((m: ResultsMenu<V>) => m.close());
 
 }
